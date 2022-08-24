5587253b29190f994608eab1fcbc1a379083fea2
Matheus Carvalho Santos <the0plxy@gmail.com>
2022-08-24 20:54:49 -0300

    Tentativa com template strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -9,8 +9,4 @@ email = "mcsantospx@gmail.com"
 profissao = "Backend Developer"
 destino = "Canada"
 
-console.log(nome)
-console.log(idade)
-console.log(email)
-console.log(profissao)
-console.log(destino)
+console.log(Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para {destino}. Confirma pra gente alguns dados? A sua idade é <valor da variável que armazena sua idade>, você é {profissao} e o seu email é {email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D);
