3d30420307fc41c2d5471641332f0a8e3321d324
Matheus Carvalho Santos <the0plxy@gmail.com>
2022-08-24 20:34:22 -0300

    Atualização da Prática

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,16 @@
 let nome
 let idade
 let email
-let profissão
+let profissao
 let destino
+nome = "Matheus"
+idade = "19"
+email = "mcsantospx@gmail.com"
+profissao = "Backend Developer"
+destino = "Canada"
+
+console.log(nome)
+console.log(idade)
+console.log(email)
+console.log(profissao)
+console.log(destino)
