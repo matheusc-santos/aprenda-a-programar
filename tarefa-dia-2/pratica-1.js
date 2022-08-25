c52f507636b073d9e00cc2e4510ccb3766a3f468
Matheus Carvalho Santos <the0plxy@gmail.com>
2022-08-25 17:02:34 -0300

    Aprendendo Template Strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,10 +3,21 @@ let idade
 let email
 let profissao
 let destino
-nome = "Matheus"
-idade = "19"
-email = "mcsantospx@gmail.com"
-profissao = "Backend Developer"
-destino = "Canada"
 
-console.log(Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para {destino}. Confirma pra gente alguns dados? A sua idade é {idade}, você é {profissao} e o seu email é {email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D);
+nome = "Matheus"
+console.log(nome)
+
+idade = "19"
+console.log(idade)
+
+email = "mcsantospx@gmail.com"
+console.log(email)
+
+profissao = "Backend Developer"
+console.log(profissao)
+
+destino = "Canada"
+console.log(destino)
+
+
+console.log((`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${destino}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`))
