50bf603b4ada0efb3d51c3247658e4790d319208
Matheus Carvalho Santos <the0plxy@gmail.com>
2022-08-27 17:00:18 -0300

    Tabuada usando loop

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,13 @@
+//Tabuada de Multiplicação do 9
+//Prática 2
+
+let n = 10
+let multiplicador = 9
+let i = 1
+
+while (i <= n){
+    let resultado = multiplicador * i
+    let textodeexibicao = `${multiplicador} X ${i} = ${resultado}`
+    console.log(textodeexibicao)
+    i++
+}
